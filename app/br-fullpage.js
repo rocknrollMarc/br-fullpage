3430f556d83a470924d2e9fc7826e58568543555
BjornRombaut <bjorn.rombaut1991@gmail.com>
2015-03-23 14:51:29 +0100

    implemented bugfixes for swiping

--- app/br-fullpage.js
+++ app/br-fullpage.js
@@ -59,6 +59,7 @@
             for (var i = 0; i<pages.length; i++){
                 angular.element(nav).append('<li><i class="br-fullpage-nav-item"></i></li>');
             }
+
             //align menu in middle
             nav.style.marginTop = (0 - (pages.length * 17)) + 'px';
             angular.element(document.getElementsByClassName('br-fullpage-nav-item')[pageIndex]).addClass('active');
@@ -66,44 +67,60 @@
                 'marginTop', '-' + pageHeight * pageIndex + 'px'
             );
 
+            //set height
+            function setHeight(){
+                angular.element(document.querySelectorAll('.br-fullpage-wrapper')).css({
+                    'height': pageHeight + 'px',
+                    'display': 'block'
+                });
+                angular.element(document.querySelectorAll('.br-fullpage')).css(
+                    'height', pageHeight + 'px'
+                );
+            }
+            setHeight();
+
             //paginate function
             function paginate(delta){
                 if (!scrolling){
+                    scrolling = true;
                     if (delta > 0) {
                         prevPage();
                     }
                     else {
                         nextPage();
                     }
-                }
-                angular.element(pages[0]).css(
-                    'marginTop', '-' + pageHeight * pageIndex + 'px'
-                );
-                angular.element(document.getElementsByClassName('br-fullpage-nav-item')).removeClass('active');
-                angular.element(document.getElementsByClassName('br-fullpage-nav-item')[pageIndex]).addClass('active');
-                sessionStorage.setItem('br-fullpage-index', pageIndex);
+                    angular.element(pages[0]).css(
+                        'marginTop', '-' + pageHeight * pageIndex + 'px'
+                    );
+                    angular.element(document.getElementsByClassName('br-fullpage-nav-item')).removeClass('active');
+                    angular.element(document.getElementsByClassName('br-fullpage-nav-item')[pageIndex]).addClass('active');
+                    sessionStorage.setItem('br-fullpage-index', pageIndex);
 
-                setTimeout(function () {
-                    scrolling = false;
-                }, 1200);
+                    setTimeout(function () {
+                        scrolling = false;
+                    }, 1000);
+                }
             }
 
             function prevPage() {
                 if (pageIndex !== 0) {
-                    scrolling = true;
                     pageIndex--;
                 }
             }
 
             function nextPage() {
                 if (pageIndex < (pages.length-1)){
-                    scrolling = true;
                     pageIndex++;
                 }
             }
 
+            function getEvent(e) {
+                return window.event || e.originalEvent || e;
+            }
+
             function mouseScroll(e){
-                var event = window.event || e.originalEvent || e;
+                var event = getEvent(e);
+                event.preventDefault();
                 var delta = event.detail? event.detail*(-120) : event.wheelDelta;
                 paginate(delta);
             }
@@ -111,18 +128,29 @@
             var startTouchY,
                 swiping = false;
             function startTouch(e){
+                var event = getEvent(e);
                 if (!swiping) {
                     swiping = true;
-                    startTouchY = e.changedTouches[0].pageY;
+                    startTouchY = event.changedTouches[0].pageY;
                 }
             }
             function endTouch(e){
-                var endTouchY = e.changedTouches[0].pageY;
+                var event = getEvent(e);
+                var endTouchY = event.changedTouches[0].pageY;
                 if (swiping && endTouchY != startTouchY) {
+                    event.preventDefault();
                     swiping = false;
                     var delta = endTouchY - startTouchY;
                     paginate(delta);
                 }
+            }
+
+            function resize(){
+                pageHeight = $window.innerHeight;
+                angular.element(pages[0]).css(
+                    'marginTop', '-' + pageHeight * pageIndex + 'px'
+                );
+                setHeight();
             }
 
             //Event Bindings
@@ -138,12 +166,20 @@
             angular.element(document).bind("pointermove", endTouch); //Mobile
             angular.element(document).bind("MSPointerMove", endTouch); //Mobile
 
-            //on resize reset pageHeight
-            angular.element($window).bind("resize", function(){
-                pageHeight = $window.innerHeight;
-                angular.element(pages[0]).css(
-                    'marginTop', '-' + pageHeight * pageIndex + 'px'
-                );
+            angular.element($window).bind("resize", resize);
+
+            //Event Destroy
+            $scope.$on('$destroy', function(){
+                angular.element(document).unbind("mousewheel", mouseScroll); //IE9, Chrome, Safari, Opera
+                angular.element(document).unbind("onmousewheel", mouseScroll); //IE 6-8
+                angular.element(document).unbind("wheel", mouseScroll); //Firefox
+                angular.element(document).unbind("touchstart", startTouch); //Mobile
+                angular.element(document).unbind("pointerdown", startTouch); //Mobile
+                angular.element(document).unbind("MSPointerDown", startTouch); //Mobile
+                angular.element(document).unbind("touchmove", endTouch); //Mobile
+                angular.element(document).unbind("pointermove", endTouch); //Mobile
+                angular.element(document).unbind("MSPointerMove", endTouch); //Mobile
+                angular.element($window).unbind("resize", resize);
             });
         }
 
